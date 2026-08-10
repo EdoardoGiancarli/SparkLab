a4c9de07b98b85895f05f04a6dc70b596bcefa33
EdoardoGiancarli <edo.giancarli@gmail.com>
2026-08-10 18:40:33 +0200

    [dev] inserted script for img/params sampling

---
+++ DevModel/sampling.py
@@ -0,0 +1,20 @@
+"""
+Script for shadowgram sampling at model inference.
+
+References:
+    * The Principles of Diffusion Models, Lai et al. (pre-release book, 2026): https://arxiv.org/abs/2510.21890
+    * Phil Wang's `denoising-diffusion-pytorch`: https://github.com/lucidrains/denoising-diffusion-pytorch/tree/main
+    * Annotated Diffusion, Niels Rogge and Kashif Rasul (with refs inside, nicely detailed)
+        https://colab.research.google.com/github/huggingface/notebooks/blob/main/examples/annotated_diffusion.ipynb#scrollTo=51d9a24c
+    * APXML website (general info gathering): https://apxml.com/courses/advanced-diffusion-architectures
+"""
+
+import torch
+from torch.types import Tensor
+
+
+__all__ = []
+
+
+
+# end
