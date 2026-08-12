4212115b7d5b33c200ec22aa8cbd258007020a8a
EdoardoGiancarli <edo.giancarli@gmail.com>
2026-08-12 02:45:14 +0200

    [dev] inserted partial arch

--- DevModel/model.py
+++ DevModel/model.py
@@ -14,6 +14,7 @@ References:
 """
 
 from functools import partial
+from typing import Optional
 
 import torch
 import torch.nn as nn
@@ -24,7 +25,7 @@ from .modules import (
     exists,
     Downsample,
     Upsample,
-    PreGroupNorm,
+    PreNorm,
     ResidualConnection,
     ResnetBlock,
     ConvNextBlock,
@@ -34,7 +35,10 @@ from .modules import (
 )
 
 
-__all__ = ['Unet', 'LOSS__TBD']
+__all__ = [
+    'Unet',
+    'LOSS__TBD',
+]
 
 
 class Unet(nn.Module):
@@ -43,8 +47,10 @@ class Unet(nn.Module):
     --source coordinates in the camera local-frame system and the estimate of the collected
     photon counts-- through diffusion.
     
-    The model accounts for Classifier-Free Guidance, provided by the extracted source point-spread
-    function and the initial values of the source parameters (coords estimate and source peak counts).
+    The model accounts for Classifier-Free Guidance, provided by the extracted source point-
+    spread function (SPSF) and the initial values of the source parameters (coords estimate
+    and source peak counts). Conditioning is performed by injecting the 2D SPSF in the model
+    and the source params, which are attended by cross-attention modules.
 
     Args:
         ...
@@ -52,206 +58,173 @@ class Unet(nn.Module):
     ## Architecture:
         ...
     """
-    ...
-
-
-
-
-def project() -> None:
-    """NOTE: placeholder"""
-    ...
-
-def prob_mask_like() -> None:
-    """NOTE: placeholder"""
-    ...
-
-def repeat() -> None:
-    """NOTE: placeholder"""
-    ...
-
-def rearrange() -> None:
-    """NOTE: placeholder"""
-    ...
-
-
-class Unet(nn.Module):
     def __init__(
         self,
-        dim,
-        num_classes,
-        cond_drop_prob = 0.5,
-        init_dim = None,
-        out_dim = None,
-        dim_mults=(1, 2, 4, 8),
-        channels = 3,
-        attn_dim_head = 32,
-        attn_heads = 4
-    ):
+        dim: int,
+        in_channels: int = 3,
+        init_dim: Optional[int] = None,
+        out_channels: Optional[int] = None,
+        dim_mults: tuple[int, ...] = (1, 2, 4, 8),
+        bottleneck_blocks: int = 1,
+        drop_cond_prob: float = 0.5,
+        cond_img_channels: int = 3,   # NOTE: to handle; condition will be SPSF + src params (tuple[Tensor, Tensor])
+        cond_dim: int = 256,          # NOTE: to handle (out proj-channels in cross-attn)
+        attn_dim_head: int = 32,
+        attn_heads: int = 4,
+        with_time_emb: bool = True,
+        use_convnext: bool = True,
+        convnext_mult: int | None = 2,
+        resnet_block_groups: int | None = 8,
+        rmsnorm: bool = True,
+    ) -> None:
         super().__init__()
-
-        # classifier free guidance stuff
-        self.cond_drop_prob = cond_drop_prob
-
         # determine dimensions
-        self.channels = channels
-        input_channels = channels
-
         init_dim = init_dim if exists(init_dim) else dim
-        self.init_conv = nn.Conv2d(input_channels, init_dim, 7, padding = 3)
+        self.init_conv = nn.Conv2d(in_channels + cond_img_channels, init_dim, 7, padding = 3)  # NOTE: signal + cond proj (for now cond is only SPSF) 
 
         dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
         in_out = list(zip(dims[:-1], dims[1:]))
 
+        # arch blocks
+        if use_convnext:
+            block_klass = partial(ConvNextBlock, mult=convnext_mult, rmsnorm=rmsnorm)
+        else:
+            block_klass = partial(ResnetBlock, groups=resnet_block_groups, rmsnorm=rmsnorm)
+
         # time embeddings
-        time_dim = dim * 4
-        self.time_mlp = TimeEmbedding(dim, time_dim)
+        if with_time_emb:
+            time_dim = 4 * dim
+            self.time_mlp = TimeEmbedding(dim, time_dim)
+        else:
+            time_dim = None
+            self.time_mlp = lambda x: None
 
-        # class embeddings
-        self.classes_emb = nn.Embedding(num_classes, dim)
-        self.null_classes_emb = nn.Parameter(torch.randn(dim))
-
-        classes_dim = dim * 4
-
-        self.classes_mlp = nn.Sequential(
-            nn.Linear(dim, classes_dim),
-            nn.GELU(),
-            nn.Linear(classes_dim, classes_dim)
-        )
+        # classifier-free guidance
+        self.drop_cond_prob = drop_cond_prob
+        self.proj_cond = nn.Conv2d(cond_img_channels, cond_dim, 1)
 
         # layers
         self.downs = nn.ModuleList([])
+        self.bnecks = nn.ModuleList([])
         self.ups = nn.ModuleList([])
         num_resolutions = len(in_out)
 
+        # - encoder
         for ind, (dim_in, dim_out) in enumerate(in_out):
             is_last = ind >= (num_resolutions - 1)
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        ResidualConnection(PreNorm(dim_in, LinearAttention(dim_in, rmsnorm=rmsnorm))),
+                        ResidualConnection(PreNorm(dim_in, LinearAttention(dim_in, context_dim=cond_dim, rmsnorm=rmsnorm))),
+                        Downsample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
 
-            self.downs.append(nn.ModuleList([
-                ResnetBlock(dim_in, dim_in, time_emb_dim = time_dim, classes_emb_dim = classes_dim),
-                ResnetBlock(dim_in, dim_in, time_emb_dim = time_dim, classes_emb_dim = classes_dim),
-                ResidualConnection(PreGroupNorm(dim_in, LinearAttention(dim_in))),
-                Downsample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out, 3, padding = 1)
-            ]))
-
+        # - bottleneck
         mid_dim = dims[-1]
-        self.mid_block1 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim, classes_emb_dim = classes_dim)
-        self.mid_attn = ResidualConnection(PreGroupNorm(mid_dim, Attention(mid_dim, dim_head = attn_dim_head, heads = attn_heads)))
-        self.mid_block2 = ResnetBlock(mid_dim, mid_dim, time_emb_dim = time_dim, classes_emb_dim = classes_dim)
+        for _ in range(bottleneck_blocks):
+            self.bnecks.append(
+                nn.ModuleList(
+                    [
+                        block_klass(mid_dim, mid_dim, time_emb_dim=time_dim),
+                        ResidualConnection(PreNorm(mid_dim, Attention(mid_dim, heads=attn_heads, dim_head=attn_dim_head))),
+                        ResidualConnection(PreNorm(mid_dim, Attention(mid_dim, heads=attn_heads, dim_head=attn_dim_head, context_dim=cond_dim))),
+                        block_klass(mid_dim, mid_dim, time_emb_dim=time_dim),
+                    ]
+                )
+            )
 
+        # - decoder
         for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
-            is_last = ind == (len(in_out) - 1)
+            is_last = ind >= (num_resolutions - 1)
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        ResidualConnection(PreNorm(dim_out, LinearAttention(dim_out, rmsnorm=rmsnorm))),
+                        ResidualConnection(PreNorm(dim_out, LinearAttention(dim_out, context_dim=cond_dim, rmsnorm=rmsnorm))),
+                        Upsample(dim_out, dim_in) if not is_last else nn.Conv2d(dim_out, dim_in, 3, padding=1),
+                    ]
+                )
+            )
 
-            self.ups.append(nn.ModuleList([
-                ResnetBlock(dim_out + dim_in, dim_out, time_emb_dim = time_dim, classes_emb_dim = classes_dim),
-                ResnetBlock(dim_out + dim_in, dim_out, time_emb_dim = time_dim, classes_emb_dim = classes_dim),
-                ResidualConnection(PreGroupNorm(dim_out, LinearAttention(dim_out))),
-                Upsample(dim_out, dim_in) if not is_last else  nn.Conv2d(dim_out, dim_in, 3, padding = 1)
-            ]))
+        out_chs = out_channels if exists(out_channels) else in_channels
+        self.final_res_block = block_klass(2 * init_dim, init_dim, time_emb_dim=time_dim)
+        self.final_conv = nn.Conv2d(init_dim, out_chs, 1)
 
-        self.out_dim = out_dim if exists(out_dim) else channels
-
-        self.final_res_block = ResnetBlock(init_dim * 2, init_dim, time_emb_dim = time_dim, classes_emb_dim = classes_dim)
-        self.final_conv = nn.Conv2d(init_dim, self.out_dim, 1)
-
-    def forward_with_cond_scale(
+    def forward_with_cfg(
         self,
-        *args,
-        cond_scale = 1.,
-        rescaled_phi = 0.,
-        remove_parallel_component = True,
-        keep_parallel_frac = 0.,
-        **kwargs
-    ):       
-        logits = self.forward(*args, cond_drop_prob = 0., **kwargs)
+    ) -> tuple[Tensor, Tensor]:
+        """
+        Applies model to noisy image at given timestep with CFG following input condition.
 
-        if cond_scale == 1:
-            return logits
-
-        null_logits = self.forward(*args, cond_drop_prob = 1., **kwargs)
-        update = logits - null_logits
-
-        if remove_parallel_component:
-            parallel, orthog = project(update, logits)
-            update = orthog + parallel * keep_parallel_frac
-
-        scaled_logits = logits + update * (cond_scale - 1.)
-
-        if rescaled_phi == 0.:
-            return scaled_logits, null_logits
-
-        std_fn = partial(torch.std, dim = tuple(range(1, scaled_logits.ndim)), keepdim = True)
-        rescaled_logits = scaled_logits * (std_fn(logits) / std_fn(scaled_logits))
-        interpolated_rescaled_logits = rescaled_logits * rescaled_phi + scaled_logits * (1. - rescaled_phi)
-
-        return interpolated_rescaled_logits, null_logits
+        Args:
+            ...
+        
+        Returns:
+            ...
+        """
+        raise NotImplementedError('to be inserted')
 
     def forward(
         self,
-        x,
-        time,
-        classes,
-        cond_drop_prob = None
-    ):
-        batch, device = x.shape[0], x.device
+        x: Tensor,
+        time: Tensor,
+        condition: Tensor,
+        drop_cond_prob: Optional[float] = None,
+    ) -> Tensor:
+        # CFG logic - to insert
+        cond_tokens = self.proj_cond(condition)
+        # NOTE: flatten spatial dimensions to sequence: (B, cond_dim, 30, 50) -> (B, 1500, cond_dim)
+        # NOTE: try do `cond_tokens.shape == (B, 1, H * W, cond_dim) or (B, 1, cond_dim, H * W)`,
+        #       but check with Attention mechanisms
 
-        cond_drop_prob = cond_drop_prob if exists(cond_drop_prob) else self.cond_drop_prob
-
-        # derive condition, with condition dropout for classifier free guidance
-
-        classes_emb = self.classes_emb(classes)
-
-        if cond_drop_prob > 0:
-            keep_mask = prob_mask_like((batch,), 1 - cond_drop_prob, device = device)
-            null_classes_emb = repeat(self.null_classes_emb, 'd -> b d', b = batch)
-
-            classes_emb = torch.where(
-                rearrange(keep_mask, 'b -> b 1'),
-                classes_emb,
-                null_classes_emb
-            )
-
-        c = self.classes_mlp(classes_emb)
-
-        # unet
-
+        # apply arch
+        x = torch.cat([x, condition], dim=1)
         x = self.init_conv(x)
         r = x.clone()
-
         t = self.time_mlp(time)
+        h: list[Tensor] = []
 
-        h = []
-
-        for block1, block2, attn, downsample in self.downs:
-            x = block1(x, t, c)
+        # - encoder
+        for block1, block2, attn, cross_attn, downsample in self.downs:
+            x = block1(x, t)
             h.append(x)
 
-            x = block2(x, t, c)
+            x = block2(x, t)
             x = attn(x)
+            x = cross_attn(x, context=cond_tokens)
             h.append(x)
 
             x = downsample(x)
 
-        x = self.mid_block1(x, t, c)
-        x = self.mid_attn(x)
-        x = self.mid_block2(x, t, c)
-
-        for block1, block2, attn, upsample in self.ups:
-            x = torch.cat((x, h.pop()), dim = 1)
-            x = block1(x, t, c)
-
-            x = torch.cat((x, h.pop()), dim = 1)
-            x = block2(x, t, c)
+        # - bottleneck
+        for block1, attn, cross_attn, block2 in self.bnecks:
+            x = block1(x, t)
             x = attn(x)
+            x = cross_attn(x, context=cond_tokens)
+            x = block2(x, t)
+
+        # - decoder
+        for block1, block2, attn, cross_attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+            x = attn(x)
+            x = cross_attn(x, context=cond_tokens) 
 
             x = upsample(x)
 
-        x = torch.cat((x, r), dim = 1)
-
-        x = self.final_res_block(x, t, c)
+        x = torch.cat((x, r), dim=1)
+        x = self.final_res_block(x, t)
         return self.final_conv(x)
-
-
-
 
 
 # end
