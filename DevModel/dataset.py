ec46f533c274bd2481deb911bca9d9212d575d88
EdoardoGiancarli <edo.giancarli@gmail.com>
2026-08-18 15:19:26 +0200

    [dev] added normalisation fns for source parameters

--- DevModel/dataset.py
+++ DevModel/dataset.py
@@ -2,6 +2,8 @@
 Script for joint diffusion dataset handling.
 """
 
+import math
+
 import torch
 from torch.types import Tensor
 
@@ -17,9 +19,30 @@ def normalise_sgs() -> Tensor:
         ...
     
     Returns:
-        out (Tensor): Normalised source shadowgrams images.
+        out (Tensor): Normalised source shadowgram images.
     """
     pass
+
+
+def symm_norm(x: Tensor, edge: float, amax: float = 1.0) -> Tensor:
+    """Normalises tensor from physical bounds `[-edge, edge]` to `[-amax, amax]`."""
+    return (abs(amax) / abs(edge)) * x
+
+
+def inverse_symm_norm(x: Tensor, edge: float, amax: float = 1.0) -> Tensor:
+    """Inverse tensor normalisation from `[-amax, amax]` back to `[-edge, edge]`."""
+    return (abs(edge) / abs(amax)) * x
+
+
+def log_snr_norm(x: Tensor, var: Tensor, max_snr: float = 2e3) -> Tensor:
+    """Normalises photon counts `x` through Log-SNR transform using variance `var`."""
+    return torch.log1p(x / torch.sqrt(var)) / math.log1p(max_snr)
+
+
+def inverse_log_snr_norm(x: Tensor, var: Tensor, max_snr: float = 2e3) -> Tensor:
+    """Inverse normalized Log-SNR back to physical photon counts."""
+    cts = torch.sqrt(var) * torch.expm1(math.log1p(max_snr) * x)
+    return cts.clamp(min=0.0)
 
 
 def normalise_params() -> Tensor:
@@ -27,14 +50,15 @@ def normalise_params() -> Tensor:
     Normalises source params in dataset.
     The source coordinates are normalised in [-1, 1] mirroring the LEM-X cameras
     FoV, expressed in the instruments local-frame system.
-    The collected photons are normalised by applying first a log-transform, and
-    then a z-score norm to prevent large values dominance.
+    The collected photons are normalised by applying a log-transform to the SNR,
+    using the detector instrumental variance and normalising in [0, 1] through
+    a max SNR-value (default to `SNRmax = 2e3`).
 
     Args:
         ...
     
     Returns:
-        out (Tensor): Normalised source parameter arrays.
+        out (Tensor): Normalised source parameters array.
     """
     pass
 
